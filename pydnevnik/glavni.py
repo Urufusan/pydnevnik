b875db4038a1d10b49ca0c082a4dc8654a1de259
Urufusan <63711492+Urufusan@users.noreply.github.com>
2023-10-30 14:27:55 +0100

    Added external creds support

--- pydnevnik/glavni.py
+++ pydnevnik/glavni.py
@@ -14,10 +14,10 @@ def force_logout(t_d_headders : dict, t_d_old_cookies : dict):
                     ))
 
 
-def main(t_id_gradebook_class : int|str = 0, bl_print_markdown = False, BL_DEBUG_MODE : bool = False) -> list[dict]:
+def main(t_id_gradebook_class : int|str = 0, bl_print_markdown = False, t_l_login_data : list[str, str] = ["email", "password"], BL_DEBUG_MODE : bool = False) -> list[dict]:
     t_id_gradebook_class = ''.join(filter(str.isnumeric, str(t_id_gradebook_class)))
     #force_logout({}, {}, {})
-    carnet_login_data = login_creds_parser(debugprint=BL_DEBUG_MODE)
+    carnet_login_data = login_creds_parser(debugprint=BL_DEBUG_MODE) if t_l_login_data == ["email", "password"] else t_l_login_data
     s = requests.session()
     s.headers.update({"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0", "DNT": "1", "Sec-GPC": "1"})
     glavniobj = s.get("https://ocjene.skole.hr/login",)
