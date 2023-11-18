cdb84c99705fc382858f9e9fb18994ddbdb8ce1b
Urufusan <63711492+Urufusan@users.noreply.github.com>
2023-11-18 23:19:29 +0100

    Added support for negative grades (F or 1)

--- tester.py
+++ tester.py
@@ -1,6 +1,9 @@
 from pprint import pprint
+
+from pydnevnik.dnevnikhelperlib import grades_exporter
 if __name__ == "__main__":
     import pydnevnik
     # pydnevnik.mainclass
     # pydnevnik.helper
-    pprint(pydnevnik.main())
+    # pprint(pydnevnik.main())
+    print(grades_exporter(pydnevnik.main()[1]))
